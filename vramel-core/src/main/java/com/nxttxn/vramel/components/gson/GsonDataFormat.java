10c11d81662d95fd3ebe966d1034f1c731fade20
Peter Long <peter.long@hs2solutions.com>
2014-04-21 21:22:30 -0400

    Add type converters to the Gson builder for LocalDate, DateTime and Instant.
    - rename gson property to gsonMarshaller
    - defined getGsonBuilderWithCommonAttributes() that is used to initialize
      the GsonBuilder with attributes common to the marshall and unmarshall
      configuration.
    - Declare LocalDateTypeConverter, DateTimeTypeConverter, and
      InstantTypeConverter and register them with the Gson parser used for
      marshaling and unmarshaling.
    - Define getGsonUnmarshaller() that lazy loads the GSon instance used for
      unmarshalling

--- vramel-core/src/main/java/com/nxttxn/vramel/components/gson/GsonDataFormat.java
+++ vramel-core/src/main/java/com/nxttxn/vramel/components/gson/GsonDataFormat.java
@@ -1,14 +1,21 @@
 package com.nxttxn.vramel.components.gson;
 
 import com.google.gson.*;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.JsonWriter;
 import com.nxttxn.vramel.Exchange;
 import com.nxttxn.vramel.Expression;
 import com.nxttxn.vramel.builder.ExpressionBuilder;
 import com.nxttxn.vramel.spi.DataFormat;
 import com.nxttxn.vramel.util.IOHelper;
+import org.joda.time.DateTime;
+import org.joda.time.Instant;
+import org.joda.time.LocalDate;
 
 import java.io.*;
 import java.lang.reflect.Type;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -21,7 +28,8 @@ import java.util.Map;
  */
 public class GsonDataFormat implements DataFormat {
     private Expression unmarshalTypeExpression;
-    private Gson gson;
+    private Gson gsonMarshaller;
+    private Gson gsonUnmarshaller;
 
     private List<ExclusionStrategy> exclusionStrategies;
     private LongSerializationPolicy longSerializationPolicy;
@@ -54,14 +62,14 @@ public class GsonDataFormat implements DataFormat {
      * @param unmarshalTypeExpression the custom unmarshal type
      */
     public GsonDataFormat(Gson gson, Expression unmarshalTypeExpression) throws Exception {
-        this.gson = gson;
+        this.gsonMarshaller = gson;
         this.unmarshalTypeExpression = unmarshalTypeExpression;
     }
 
     @Override
     public void marshal(Exchange exchange, Object graph, OutputStream stream) throws Exception {
         BufferedWriter writer = IOHelper.buffered(new OutputStreamWriter(stream));
-        getGson().toJson(graph, writer);
+        getGsonMarshaller().toJson(graph, writer);
         writer.close();
     }
 
@@ -69,17 +77,23 @@ public class GsonDataFormat implements DataFormat {
     public Object unmarshal(Exchange exchange, InputStream stream) throws Exception {
         Class unmarshalType = this.unmarshalTypeExpression.evaluate(exchange, Class.class);
         BufferedReader reader = IOHelper.buffered(new InputStreamReader(stream));
-        //Object result = getGson().fromJson(reader, unmarshalType);
 
-        final Gson hgson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create();
-        Object result = hgson.fromJson(reader, unmarshalType);
+        Object result = getGsonUnmarshaller().fromJson(reader, unmarshalType);
 
         reader.close();
         return result;
     }
 
-    protected void buildDefaultGson() throws Exception {
-        GsonBuilder builder = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES);
+    protected GsonBuilder getGsonBuilderWithCommonAttributes() {
+        return new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss")
+                                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+                                .registerTypeAdapter(LocalDate.class, new LocalDateTypeConverter())
+                                .registerTypeAdapter(DateTime.class, new DateTimeTypeConverter())
+                                .registerTypeAdapter(Instant.class, new InstantTypeConverter());
+    }
+
+    protected void buildDefaultGsonMarshaller() throws Exception {
+        GsonBuilder builder = getGsonBuilderWithCommonAttributes();
         if (exclusionStrategies != null && !exclusionStrategies.isEmpty()) {
             ExclusionStrategy[] strategies = exclusionStrategies.toArray(new ExclusionStrategy[exclusionStrategies.size()]);
             builder.setExclusionStrategies(strategies);
@@ -105,7 +119,7 @@ public class GsonDataFormat implements DataFormat {
 //        if (polymorphic != null && polymorphic) {
 //            builder.registerTypeAdapter(unmarshalType, new InheritanceAdapter<Object>());
 //        }
-        gson =  builder.create();
+        gsonMarshaller =  builder.create();
     }
 
 
@@ -176,11 +190,18 @@ public class GsonDataFormat implements DataFormat {
         this.dateFormatPattern = dateFormatPattern;
     }
 
-    public Gson getGson() throws Exception {
-        if (gson == null) {
-            buildDefaultGson();
+    public Gson getGsonMarshaller() throws Exception {
+        if (gsonMarshaller == null) {
+            buildDefaultGsonMarshaller();
         }
-        return this.gson;
+        return this.gsonMarshaller;
+    }
+
+    protected Gson getGsonUnmarshaller() {
+        if ( gsonUnmarshaller == null) {
+            gsonUnmarshaller = getGsonBuilderWithCommonAttributes().create();
+        }
+        return gsonUnmarshaller;
     }
 
     public void setPolymorphic(Boolean polymorphic) {
@@ -230,4 +251,60 @@ public class GsonDataFormat implements DataFormat {
         }
 
     }
+
+    protected static class LocalDateTypeConverter implements JsonDeserializer<LocalDate>, JsonSerializer<LocalDate> {
+
+        @Override
+        public JsonElement serialize(LocalDate src, Type type, JsonSerializationContext context) {
+            return new JsonPrimitive(src.toString());
+        }
+
+        @Override
+        public LocalDate deserialize(JsonElement json, Type type, JsonDeserializationContext context) throws JsonParseException {
+            try {
+                return new LocalDate(json.getAsString());
+            } catch (IllegalArgumentException e) {
+                try {
+                    // Maybe this is in Date format
+                    Date date = context.deserialize(json, Date.class);
+                    return new LocalDate(date.getTime());
+                } catch (JsonParseException p) {
+                    throw new JsonParseException("Invalid date \""+json.getAsString()+"\"", e);
+                }
+            }
+        }
+    }
+
+    protected static class DateTimeTypeConverter implements JsonSerializer<DateTime>, JsonDeserializer<DateTime> {
+        @Override
+        public JsonElement serialize(DateTime src, Type srcType, JsonSerializationContext context) {
+            return new JsonPrimitive(src.toString());
+        }
+
+        @Override
+        public DateTime deserialize(JsonElement json, Type type, JsonDeserializationContext context)
+                throws JsonParseException {
+            try {
+                return new DateTime(json.getAsString());
+            } catch (IllegalArgumentException e) {
+                // May be it came in formatted as a java.util.Date, so try that
+                Date date = context.deserialize(json, Date.class);
+                return new DateTime(date);
+            }
+        }
+    }
+
+    protected static class InstantTypeConverter implements JsonSerializer<Instant>, JsonDeserializer<Instant> {
+        @Override
+        public JsonElement serialize(Instant src, Type srcType, JsonSerializationContext context) {
+            return new JsonPrimitive(src.getMillis());
+        }
+
+        @Override
+        public Instant deserialize(JsonElement json, Type type, JsonDeserializationContext context)
+                throws JsonParseException {
+            return new Instant(json.getAsLong());
+        }
+    }
+
 }
